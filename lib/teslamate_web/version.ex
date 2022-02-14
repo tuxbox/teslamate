f3551107802305230c0240b2248b32aff1005770
Iulius <iulius@gutberlet.eu>
2022-02-14 13:47:50 +0100

    first try to implement a version endpoint

--- lib/teslamate_web/version.ex
+++ lib/teslamate_web/version.ex
@@ -1,12 +1,20 @@
 defmodule TeslaMateWeb.Version do
   import Plug.Conn
+  import File
 
   def init(opts), do: opts
 
   def call(%Plug.Conn{request_path: "/version"} = conn, _opts) do
-    conn
-    |> send_resp(200, "<<VERSION>>")
-    |> halt()
+    case File.read("../../VERSION") do
+      {:ok, body}      -> # do something with the `body`
+        conn
+        |> send_resp(200, body)
+        |> halt()
+      {:error, reason} -> # handle the error caused by `reason`
+        conn
+        |> send_resp(500, reason)
+        |> halt()
+    end
   end
 
   def call(conn, _opts), do: conn
